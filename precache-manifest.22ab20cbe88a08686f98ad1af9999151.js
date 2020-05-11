6e6c413396844f3839b0c7602d546cdb24684276
unknown <you@example.com>
2020-05-11 12:32:10 -0700

    Updates

---
+++ precache-manifest.22ab20cbe88a08686f98ad1af9999151.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a4c2f4a25bacbeb92dfc",
+    "url": "/amazonreact/static/css/main.27f78ebf.chunk.css"
+  },
+  {
+    "revision": "a4c2f4a25bacbeb92dfc",
+    "url": "/amazonreact/static/js/main.f03c64eb.chunk.js"
+  },
+  {
+    "revision": "eb1f54d552c9885227a0",
+    "url": "/amazonreact/static/js/runtime~main.81137e4a.js"
+  },
+  {
+    "revision": "27901928354372bbbf89",
+    "url": "/amazonreact/static/js/2.56acc587.chunk.js"
+  },
+  {
+    "revision": "a8ad8586b8d56207ee58f8301e77596c",
+    "url": "/amazonreact/static/media/heartbeat.a8ad8586.png"
+  },
+  {
+    "revision": "c883216d3c14493c0a12b66d354f9f68",
+    "url": "/amazonreact/index.html"
+  }
+];
